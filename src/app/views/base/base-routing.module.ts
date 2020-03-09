356199cf187ba83c142a2f2ae3dafc35ca70ca3d
wei <sagasw@gmail.com>
2020-03-09 18:46:10 +0800

    clean up

--- src/app/views/base/base-routing.module.ts
+++ src/app/views/base/base-routing.module.ts
@@ -3,15 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { CardsComponent } from './cards.component';
 import { FormsComponent } from './forms.component';
-import { SwitchesComponent } from './switches.component';
 import { TablesComponent } from './tables.component';
-import { TabsComponent } from './tabs.component';
-import { CarouselsComponent } from './carousels.component';
-import { CollapsesComponent } from './collapses.component';
-import { PaginationsComponent } from './paginations.component';
 import { PopoversComponent } from './popovers.component';
-import { ProgressComponent } from './progress.component';
-import { TooltipsComponent } from './tooltips.component';
 import { NavbarsComponent } from './navbars/navbars.component';
 
 const routes: Routes = [
@@ -40,13 +33,6 @@ const routes: Routes = [
         }
       },
       {
-        path: 'switches',
-        component: SwitchesComponent,
-        data: {
-          title: 'Switches'
-        }
-      },
-      {
         path: 'tables',
         component: TablesComponent,
         data: {
@@ -54,52 +40,10 @@ const routes: Routes = [
         }
       },
       {
-        path: 'tabs',
-        component: TabsComponent,
-        data: {
-          title: 'Tabs'
-        }
-      },
-      {
-        path: 'carousels',
-        component: CarouselsComponent,
-        data: {
-          title: 'Carousels'
-        }
-      },
-      {
-        path: 'collapses',
-        component: CollapsesComponent,
-        data: {
-          title: 'Collapses'
-        }
-      },
-      {
-        path: 'paginations',
-        component: PaginationsComponent,
-        data: {
-          title: 'Pagination'
-        }
-      },
-      {
         path: 'popovers',
         component: PopoversComponent,
         data: {
           title: 'Popover'
-        }
-      },
-      {
-        path: 'progress',
-        component: ProgressComponent,
-        data: {
-          title: 'Progress'
-        }
-      },
-      {
-        path: 'tooltips',
-        component: TooltipsComponent,
-        data: {
-          title: 'Tooltips'
         }
       },
       {
--- src/app/views/base/base.module.ts
+++ src/app/views/base/base.module.ts
@@ -8,20 +8,16 @@ import { CardsComponent } from './cards.component';
 // Forms Component
 import { FormsComponent } from './forms.component';
 
-import { SwitchesComponent } from './switches.component';
 import { TablesComponent } from './tables.component';
 
 // Tabs Component
 import { TabsModule } from 'ngx-bootstrap/tabs';
-import { TabsComponent } from './tabs.component';
 
 // Carousel Component
 import { CarouselModule } from 'ngx-bootstrap/carousel';
-import { CarouselsComponent } from './carousels.component';
 
 // Collapse Component
 import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { CollapsesComponent } from './collapses.component';
 
 // Dropdowns Component
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
@@ -32,16 +28,12 @@ import { PopoversComponent } from './popovers.component';
 
 // Popover Component
 import { PopoverModule } from 'ngx-bootstrap/popover';
-import { PaginationsComponent } from './paginations.component';
 
 // Progress Component
 import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
-import { ProgressComponent } from './progress.component';
 
 // Tooltip Component
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { TooltipsComponent } from './tooltips.component';
-
 // navbars
 import { NavbarsComponent } from './navbars/navbars.component';
 
@@ -65,15 +57,8 @@ import { BaseRoutingModule } from './base-routing.module';
   declarations: [
     CardsComponent,
     FormsComponent,
-    SwitchesComponent,
     TablesComponent,
-    TabsComponent,
-    CarouselsComponent,
-    CollapsesComponent,
-    PaginationsComponent,
     PopoversComponent,
-    ProgressComponent,
-    TooltipsComponent,
     NavbarsComponent
   ]
 })
